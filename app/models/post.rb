41248addaa8c40547c663bef60f6dc50a22b787a
Blaire Baker <bakerblaire@gmail.com>
2020-12-31 13:24:09 -0600

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,14 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :post_tags
   has_many :tags, :through => :post_tags
+  has_many :comments
   accepts_nested_attributes_for :tags, reject_if: lambda {|attributes| attributes['name'].blank?}
   validates_presence_of :name, :content
+
+  def destroy
+    self.comments.each do |comment|
+      comment.delete
+    end
+    self.delete
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513204421) do
+ActiveRecord::Schema.define(version: 20201231185620) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string  "content"
+    t.integer "post_id"
+    t.integer "user_id"
+  end
 
   create_table "post_tags", force: :cascade do |t|
     t.integer  "post_id"
